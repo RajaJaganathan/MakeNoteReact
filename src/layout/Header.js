800cb989fbc47b5c9466b7c3592ec0019df8fdcb
Raja J <raja_j@apple.com>
2017-06-10 21:28:36 +0530

    refactored header component

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,11 +1,10 @@
-import React, {Component} from 'react';
-import {Link, IndexLink} from 'react-router';
-
+import React, { Component } from "react";
+import { Link, IndexLink } from "react-router";
 
 export default class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = { filterText: '' };
+    this.state = { filterText: "" };
     this.onFilterNote = this.onFilterNote.bind(this);
   }
 
@@ -18,42 +17,83 @@ export default class Header extends Component {
     this.props.onSortBy(field);
   }
 
-  render() {
-    return (<nav className="navbar navbar-default" role="navigation">
-      <div className="navbar-header">
-        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-          <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
-          <span className="icon-bar"></span>
-        </button>
+  _renderSearchForm() {
+    return (
+      <form className="navbar-form navbar-left" role="search">
+        <div className="form-group filter-field">
+          <input
+            type="text"
+            value={this.state.filterText}
+            onChange={this.onFilterNote}
+            className="form-control"
+            placeholder="Search by title"
+          />
+        </div>
+        <button type="submit" className="btn btn-default">Submit</button>
+      </form>
+    );
+  }
 
-        <a className="navbar-brand" href="#">
-          <IndexLink to="/" activeClassName="active">Make Note</IndexLink>
-        </a>
-      </div>
-      <div className="collapse navbar-collapse navbar-ex1-collapse">
-        <ul className="nav navbar-nav">
-          <li className={location.pathname === ('/') && 'active'}><IndexLink to="/" activeClassName="active">Notes</IndexLink></li>
-          <li className={location.pathname.startsWith('/contact') && 'active'}><Link to="/contact" activeClassName="active">Contact</Link></li>
-          <li className={location.pathname.startsWith('/aboutus') && 'active'}><Link to="/aboutus" activeClassName="active">About</Link></li>
-        </ul>
-        <form className="navbar-form navbar-left" role="search">
-          <div className="form-group filter-field">
-            <input type="text" value={this.state.filterText} onChange={this.onFilterNote } className="form-control" placeholder="Search by title"/>
-          </div>
-          <button type="submit" className="btn btn-default">Submit</button>
-        </form>
-        <ul className="nav navbar-nav navbar-right">
-          <li className="dropdown">
-            <a href="#" className="dropdown-toggle" data-toggle="dropdown">Sort By <b className="caret"></b></a>
-            <ul className="dropdown-menu">
-              <li><a onClick={(e) => this.onSortBy(e, 'title') } href="#">Title</a></li>
-              <li><a onClick={(e) => this.onSortBy(e, 'savedDate') } href="#">Dates</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </nav>)
+  _renderSortBy() {
+    return (
+      <ul className="nav navbar-nav navbar-right">
+        <li className="dropdown">
+          <a href="#" className="dropdown-toggle" data-toggle="dropdown">
+            Sort By <b className="caret" />
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <a onClick={e => this.onSortBy(e, "title")} href="#">Title</a>
+            </li>
+            <li>
+              <a onClick={e => this.onSortBy(e, "savedDate")} href="#">Dates</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    );
+  }
+
+  _renderNavbar() {
+    return (
+      <ul className="nav navbar-nav">
+        <li className={location.pathname === "/" && "active"}>
+          <IndexLink to="/" activeClassName="active">Notes</IndexLink>
+        </li>
+        <li className={location.pathname.startsWith("/contact") && "active"}>
+          <Link to="/contact" activeClassName="active">Contact</Link>
+        </li>
+        <li className={location.pathname.startsWith("/aboutus") && "active"}>
+          <Link to="/aboutus" activeClassName="active">About</Link>
+        </li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-default" role="navigation">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle"
+            data-toggle="collapse"
+            data-target=".navbar-ex1-collapse">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+          </button>
+          <a className="navbar-brand" href="#">
+            <IndexLink to="/" activeClassName="active">Make Note</IndexLink>
+          </a>
+        </div>
+        <div className="collapse navbar-collapse navbar-ex1-collapse">
+              {this._renderNavbar()}
+              {this._renderSearchForm()}
+              {this._renderSortBy()}
+        </div>
+      </nav>
+    );
   }
 }
